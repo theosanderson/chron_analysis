adf8b936bc98f0b0a1256bd937f71428f85a0ce7
Theo Sanderson <theo@sndrsn.co.uk>
2021-10-23 21:45:40 +0100

    add

--- pruning/performance.py
+++ pruning/performance.py
@@ -2,38 +2,83 @@ import os
 import pandas as pd
 import tqdm
 import time
+import subprocess
 
 output_file = open("performance.tsv", "wt")
+output_file.write("num_tips\ttime\tmemory\n")
 
-number_of_tips = [4**x for x in range(2, 12)]
+number_of_tips = [4**x for x in range(2, 20)]
 
 metadata = pd.read_csv("../public-2021-09-15.metadata.tsv.gz", sep="\t")
 
-for num_tips in tqdm.tqdm(number_of_tips):
-    os.system(
-        f"gzcat ../public-2021-09-15.all.nwk.gz | gotree prune --random {num_tips} --revert > tree.{num_tips}.nwk"
-    )
-    os.system(
-        f"cat tree.{num_tips}.nwk | gotree labels --tips > tips.{num_tips}.txt"
-    )
 
-    # scale branch lengths down by 30000 times to per-site
-    scale_factor = 1 / 30_000
-    os.system(
-        f"cat tree.{num_tips}.nwk| gotree brlen scale --factor {scale_factor} > tree_scaled.{num_tips}.nwk"
-    )
+def run_process_and_return_stderr_and_stdout(command):
+    """
+    Run a command and return stdout and stderr.
+    """
+    process = subprocess.run(command,
+                             stdout=subprocess.PIPE,
+                             stderr=subprocess.PIPE,
+                             universal_newlines=True,
+                             shell=True)
+    return process.stdout, process.stderr
 
-    tip_names = open(f"tips.{num_tips}.txt").read().splitlines()
 
-    # Create metadata filtered to strain in tip_names
-    filtered_metadata = metadata[metadata['strain'].isin(tip_names)]
-    filtered_metadata.to_csv(f"metadata.{num_tips}.tsv", sep="\t", index=False)
-
-    #Time the treetime call:
+def run_command_and_get_time_and_memory_usage(command):
+    """
+    We use /usr/bin/time to get this. Need to use stderr.
+    """
     start_time = time.time()
-    command = f"treetime clock --dates metadata.{num_tips}.tsv --tree tree_scaled.{num_tips}.nwk --sequence-length 30000  --keep-root"
-    os.system(command)
+    stdout, stderr = run_process_and_return_stderr_and_stdout(
+        ("/usr/bin/time -l " + command))
+    print(stdout)
     print(command)
+    # extract memory usage, which is maximum resident set size
+    for line in stderr.split("\n"):
+        if "maximum resident set size" in line:
+            components = line.split(" ")
+            # find the integer
+            for component in components:
+                try:
+                    mem = int(component)
+                    break
+                except:
+                    pass
+
     end_time = time.time()
-    output_file.write(f"{num_tips}\t{end_time-start_time}\n")
-    output_file.flush()
+    return end_time - start_time, mem
+
+
+def main():
+    for num_tips in tqdm.tqdm(number_of_tips):
+        os.system(
+            f"gzcat ../public-2021-09-15.all.nwk.gz | gotree prune --random {num_tips} --revert > working/tree.{num_tips}.nwk"
+        )
+        os.system(
+            f"cat working/tree.{num_tips}.nwk | gotree labels --tips > working/tips.{num_tips}.txt"
+        )
+
+        # scale branch lengths down by 30000 times to per-site
+        scale_factor = 1 / 30_000
+        os.system(
+            f"cat working/tree.{num_tips}.nwk| gotree brlen scale --factor {scale_factor} > working/tree_scaled.{num_tips}.nwk"
+        )
+
+        tip_names = open(f"working/tips.{num_tips}.txt").read().splitlines()
+
+        # Create metadata filtered to strain in tip_names
+        filtered_metadata = metadata[metadata['strain'].isin(tip_names)]
+        filtered_metadata.to_csv(f"working/metadata.{num_tips}.tsv",
+                                 sep="\t",
+                                 index=False)
+
+        #Time the treetime call:
+        start_time = time.time()
+        command = f"treetime clock --dates working/metadata.{num_tips}.tsv --tree working/tree_scaled.{num_tips}.nwk --sequence-length 30000  --keep-root --outdir ./working/"
+        timing, mem = run_command_and_get_time_and_memory_usage(command)
+        output_file.write(f"{num_tips}\t{timing}\t{mem}\n")
+        output_file.flush()
+
+
+if __name__ == "__main__":
+    main()
