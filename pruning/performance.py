3048ecb0041b667644246b08f38313e246605277
theosanderson <theo@theo.io>
2023-01-21 11:03:24 +0000

    updates with lsd perf

--- pruning/performance.py
+++ pruning/performance.py
@@ -4,6 +4,7 @@ import tqdm
 import time
 import subprocess
 import argparse
+import datetime as dt
 #create an argparser to get a single argument called mode
 #mode can be either "treetime" or "chronumental"
 
@@ -12,14 +13,16 @@ parser.add_argument('--mode',
                     type=str,
                     default='treetime',
                     help='Mode to run in. Can be treetime or chronumental',
-                    choices=['treetime', 'chronumental'])
+                    choices=['treetime', 'chronumental', 'lsd'])
 
 parser.add_argument('--gpu', action='store_true', help='Use GPU')
+
+parser.add_argument('--lsdF', action='store_true', help='Disable temporal constraints')
 
 args = parser.parse_args()
 
 output_file = open(
-    f"performance{'' if args.mode=='treetime' else '_chron'}{'' if not args.gpu else '_gpu'}.tsv",
+    f"performance_{args.mode}_{'nogpu' if not args.gpu else '_gpu'}{'_F' if args.lsdF else ''}.tsv",
     "wt")
 output_file.write("num_tips\ttime\tmemory\n")
 
@@ -62,6 +65,21 @@ def run_command_and_get_time_and_memory_usage(command):
     end_time = time.time()
     return end_time - start_time, mem
 
+def dateToFloatYear(string):
+    """
+    Convert a date string to a float year.
+    2021-09-15 -> 2021.7
+    """
+    string = string.strip()
+    node, dates = string.split("\t")
+    #print(string)
+    try: 
+        date = dt.datetime.strptime(dates, "%Y-%m-%d")
+        return node + "\t" + f"{date.year + (date.timetuple().tm_yday - 1) / 365.25}\n"
+    except ValueError as e:
+        print(f"Could not parse date {dates} - {e}")
+        return None
+
 
 def main():
     for num_tips in tqdm.tqdm(number_of_tips):
@@ -72,24 +90,58 @@ def main():
             f"cat working/tree.{num_tips}.nwk | gotree labels --tips > working/tips.{num_tips}.txt"
         )
 
+
+
         # scale branch lengths down by 30000 times to per-site
         scale_factor = 1 / 30_000
         os.system(
             f"cat working/tree.{num_tips}.nwk| gotree brlen scale --factor {scale_factor} > working/tree_scaled.{num_tips}.nwk"
         )
 
+        # open the newick file and add an outgroup called "out"
+        with open(f"working/tree_scaled.{num_tips}.nwk", "rt") as f:
+            tree = f.read()
+            # remove last semi-colon
+            tree = tree[:-1]
+        with open(f"working/tree_scaled.{num_tips}.nwk", "wt") as f:
+            rooted_tree = f"{tree}:0.0, out:0.0;"
+            f.write(rooted_tree)
+
         tip_names = open(f"working/tips.{num_tips}.txt").read().splitlines()
 
         # Create metadata filtered to strain in tip_names
         filtered_metadata = metadata[metadata['strain'].isin(tip_names)]
+        filtered_metadata = filtered_metadata[['strain', 'date']]
+        # filter out date "?"
+        filtered_metadata = filtered_metadata[filtered_metadata['date'] != "?"]
+
+        # 
         filtered_metadata.to_csv(f"working/metadata.{num_tips}.tsv",
                                  sep="\t",
                                  index=False)
+
+        # if mode is "lsd" then add the metadata num rows as the first line
+        if args.mode == 'lsd':
+            f = open(f"working/metadata.{num_tips}.tsv", "rt")
+            lines = f.readlines()
+            f.close()
+            f = open(f"working/metadata.{num_tips}.tsv", "wt")
+            
+            # remove the first line
+            lines = lines[1:]
+            #lines = [dateToFloatYear(x) for x in lines]
+            lines = [x  for x in lines if x is not None]
+            f.write(f"{len(lines)}\n")
+
+            f.writelines(lines)
+            f.close()
 
         #Time the treetime call:
         start_time = time.time()
         if args.mode == 'treetime':
             command = f"treetime --dates working/metadata.{num_tips}.tsv --tree working/tree_scaled.{num_tips}.nwk --sequence-length 30000  --keep-root --outdir ./working/"
+        elif args.mode == 'lsd':
+            command = f"lsd2 -i working/tree_scaled.{num_tips}.nwk -d working/metadata.{num_tips}.tsv -s 30000 -g outgroup_file -G {'-F' if args.lsdF else ''}"
         elif args.gpu:
             command = f"chronumental --treat_mutation_units_as_normalised_to_genome_size 30000 --dates working/metadata.{num_tips}.tsv --tree working/tree_scaled.{num_tips}.nwk --tree_out /dev/null --dates_out /dev/null --use_gpu --steps 1000"
         else:
